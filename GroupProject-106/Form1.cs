70f5e883c64acca13179c36b0a65a6d599258f37
Alexey-geek <samoiloff.alex@gmail.com>
2023-03-13 11:06:00 +0300

    Концепт класса для пре-парсинга

--- GroupProject-106/Form1.cs
+++ GroupProject-106/Form1.cs
@@ -6,5 +6,17 @@ namespace GroupProject_106
         {
             InitializeComponent();
         }
+
+        private void button1_MouseClick(object sender, MouseEventArgs e)
+        {
+            //считываем строку с интерфейса
+
+            //проверяем правильно ли написано выражение
+            if () {
+
+            //если все ок , парсим , интегрируем
+            
+            }
+        }
     }
 }

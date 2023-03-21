e712a95bd889cebc6f04fbe2d30435c3cd2052fb
Аделя <laladesi@mail.ru>
2023-03-21 20:19:38 +0300

    -fixed everything should be fixed

--- GroupProject-106/ExpressionTree.cs
+++ GroupProject-106/ExpressionTree.cs
@@ -10,32 +10,73 @@ using System.Data.Common;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using NewVersionOfParsing;
 
 namespace GroupProject_106
 {
     public class ExpressionTree
     {
-        /*private ExpressionTreeNode root;
-        public ExpressionTreeNode Root => root;
-        public ExpressionTree(string expression)
+        private ExpressionTreeNode root;
+        private List<string> parsingList { get; set; }
+        public ExpressionTree(List<string> parsingList)
         {
-            root = new ExpressionTreeNode(expression,1);
-        }*/
-        private ExpressionTreeNode Root { get; set; }
-
-        private int Count { get; set; }
-
-        public void Add(string expression)
+            this.parsingList = parsingList;
+            root = new ExpressionTreeNode(Pop());
+            CreateTree(root);
+        }
+        private void CreateTree(ExpressionTreeNode curr)
         {
-            if (Root == null)
+            if (Parsing.operators.Contains(curr.string_value))
             {
-                Root = new ExpressionTreeNode(expression);
-                Count = 1;
-                return;
+                curr.RightNode = new ExpressionTreeNode(Pop());
+                CreateTree(curr.RightNode);
+                curr.LeftNode = new ExpressionTreeNode(Pop());
+                CreateTree(curr.LeftNode);
             }
-
-            Root.Add(expression);
-            Count++;
+            if (Parsing.functions.Contains(curr.string_value))
+            {
+                curr.RightNode = new ExpressionTreeNode(Pop());
+                CreateTree(curr.RightNode);
+            }
+        }
+        public double Tarvase(double x)
+        {
+            return CountValue(root, x);
+        }
+        private double CountValue(ExpressionTreeNode elem, double x)
+        {
+            switch (elem.string_value)
+            {
+                case "+":
+                    return CountValue(elem.RightNode, x) + CountValue(elem.LeftNode, x);
+                case "-":
+                    return CountValue(elem.LeftNode, x) - CountValue(elem.RightNode, x);
+                case "*":
+                    return CountValue(elem.RightNode, x) * CountValue(elem.LeftNode, x);
+                case "/":
+                    return CountValue(elem.LeftNode, x) / CountValue(elem.RightNode, x);
+                case "^":
+                    return Math.Pow(CountValue(elem.LeftNode, x), CountValue(elem.RightNode, x));
+                case "sin":
+                    return Math.Sin(CountValue(elem.RightNode, x));
+                case "cos":
+                    return Math.Cos(CountValue(elem.RightNode, x));
+                case "tan":
+                    return Math.Tan(CountValue(elem.RightNode, x));
+                case "cot":
+                    return 1 / Math.Tan(CountValue(elem.RightNode, x));
+                case "ln":
+                    return Math.Log(CountValue(elem.RightNode, x));
+                case "x":
+                    return x;
+                default: return Convert.ToDouble(elem.string_value);
+            }
+        }
+        private string Pop()
+        {
+            string save = parsingList[parsingList.Count - 1];
+            parsingList.RemoveAt(parsingList.Count - 1);
+            return save;
         }
 
 
--- GroupProject-106/ExpressionTreeNode.cs
+++ GroupProject-106/ExpressionTreeNode.cs
@@ -22,66 +22,7 @@ namespace NewVersionOfParsing
         public ExpressionTreeNode()
         {
 
-        }
-
-
-
-
-        
-
-        public void Insert(params string[] parsed_elems)
-        {
-            for (int j = 0; j < parsed_elems.Length; j++)
-            {
-                if (parsed_elems[j].Any(item => item >= 48 && item <= 57))
-                {
-                    if (RightNode == null)
-                    {
-                        RightNode = new ExpressionTreeNode(parsed_elems[j]);
-                    }
-                }
-
-                else if (parsed_elems[j] == "x")
-                {
-                    if (LeftNode == null)
-                    {
-                        LeftNode = new ExpressionTreeNode(parsed_elems[j]);
-                    }
-                }
-
-                else if (Parsing.functions.Contains(parsed_elems[j]))
-                {
-                    if (LeftNode == null)
-                    {
-                        LeftNode = new ExpressionTreeNode(parsed_elems[j]);
-                    }
-                }
-                else if (Parsing.operators.Contains(parsed_elems[j]) && parsed_elems.Length == 2)
-                {
-                    if (RightNode == null)
-                    {
-                        RightNode = new ExpressionTreeNode(parsed_elems[0]);
-                    }
-                    if (LeftNode == null)
-                    {
-                        LeftNode = new ExpressionTreeNode(parsed_elems[1]);
-
-                    }
-                }
-                else if (Parsing.operators.Contains(parsed_elems[j]) && parsed_elems.Length == 1)
-                {
-                    if (RightNode == null)
-                    {
-                        RightNode = new ExpressionTreeNode(parsed_elems[0]);
-                    }
-                    
-                }
-            }
-
-        }
-        
-
-        
+        }  
 
     }
 }
